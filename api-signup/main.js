67f06cdc092fd8e145d49cdc55b23b1c0af6aec7
loucimf <145812460+loucimf@users.noreply.github.com>
2024-07-07 20:09:45 -0300

    Update

--- api-signup/main.js
+++ api-signup/main.js
@@ -6,22 +6,20 @@ let buttonCreateUser = document.getElementById("buttonCreate")
 buttonCreateUser.addEventListener("click", writeUserData);
 
 function getUserData() {
-    let inputName = document.getElementById("name")
-    let inputLastName = document.getElementById("lastName")
-    let inputPassword = document.getElementById("password")
-    return inputName, inputLastName, inputPassword;
+    let inputName = document.getElementById("name").value
+    let inputLastName = document.getElementById("lastName").value
+    let inputPassword = document.getElementById("password").value
+    console.log('Retrieved User Data')
+
+    return [inputName, inputLastName, inputPassword];
 }
 
 
 function writeUserData() {
     let [name, lastName, password] = getUserData()
-    dataUser.push(name)
-    dataUser.push(lastName)
-    dataUser.push(password)
-
-    console.log(name)
-    
+    dataUser.push(name, lastName, password)
+    console.log('Wrote User Data')
 }
 
 
-export { dataUser }
+export { dataUser };
