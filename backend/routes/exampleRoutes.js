7f22cda79c235d2b9dbccbc57fefdf10214c3a5a
loucimf <145812460+loucimf@users.noreply.github.com>
2024-07-07 21:13:30 -0300

    moved to frontend folder and created http server

---
+++ backend/routes/exampleRoutes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import path from 'path'
+import { Alumno } from "../models/Alumno.js";
+// import { dataUser } from '../frontend/main.js'
+
+const exampleRouter = Router()
+
+// exampleRouter.get('/', async (request, response) => {
+//     try {
+//         const alumno = await Alumno.create({
+//             name: dataUser[0],
+//             lastName: dataUser[1],
+//             password: dataUser[2]
+//         })
+//         response.status(200).send({succesful: true, result: alumno});
+//     } catch (error) {
+//         response.status(400).send('Something went wrong!');
+//     }
+// });
+
+export default exampleRouter
