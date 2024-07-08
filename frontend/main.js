29d132de95cb41704a22a87db2efd3fe05245048
loucimf <145812460+loucimf@users.noreply.github.com>
2024-07-07 22:08:13 -0300

    Attempt to call BE from frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,25 +1,48 @@
 
-let dataUser = [];
-console.log('hello')
+// let dataUser = [];
+// console.log('hello')
+// let buttonCreateUser = document.getElementById("buttonCreate")
+
+// buttonCreateUser.addEventListener("click", writeUserData);
+
+// function getUserData() {
+//     let inputName = document.getElementById("name").value
+//     let inputLastName = document.getElementById("lastName").value
+//     let inputPassword = document.getElementById("password").value
+//     console.log('Retrieved User Data')
+
+//     return [inputName, inputLastName, inputPassword];
+// }
+
+
+// function writeUserData() {
+//     let [name, lastName, password] = getUserData()
+//     dataUser.push(name, lastName, password)
+//     console.log('Wrote User Data')
+// }
+
+// Fetch data from the backend
+
 let buttonCreateUser = document.getElementById("buttonCreate")
+buttonCreateUser.addEventListener("click", getData);
 
-buttonCreateUser.addEventListener("click", writeUserData);
+function getData() {
+    console.log("hello!")
 
-function getUserData() {
-    let inputName = document.getElementById("name").value
-    let inputLastName = document.getElementById("lastName").value
-    let inputPassword = document.getElementById("password").value
-    console.log('Retrieved User Data')
-
-    return [inputName, inputLastName, inputPassword];
+    fetch('http://localhost:8080/api/test')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            // Create a new paragraph element
+            // const p = document.createElement('p');
+            // Set the text content to the 'hello' property of the item
+            // p.textContent = item.hello;
+            // Append the paragraph to the body
+            // document.body.appendChild(p);
+            console.log(item)
+        });
+    })
+    .catch(error => console.error('Error:', error));
 }
 
-
-function writeUserData() {
-    let [name, lastName, password] = getUserData()
-    dataUser.push(name, lastName, password)
-    console.log('Wrote User Data')
-}
-
-
-export { dataUser };
+// export { dataUser };
